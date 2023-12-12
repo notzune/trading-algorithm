8365c3c08f4a230283ee444362ad2d5675375f26
zeyad rashed <brwnboy2002@gmail.com>
2023-12-11 20:03:41 -0500

    feat: created a fine-tuning script
    this creates a modularized function that i can continue to reuse

--- fine-tuning.py
+++ fine-tuning.py
@@ -1,169 +1,48 @@
-import re
-
-import torch
-from datasets import load_dataset, DatasetDict
 from torch.optim import AdamW
-from torch.utils.data import DataLoader
-from transformers import AutoModelForSequenceClassification, AutoTokenizer
-
-# Load the dataset with a specific configuration
-dataset = load_dataset("financial_phrasebank", "sentences_allagree", download_mode="force_redownload")
-# dataset = load_dataset("nickmuchi/financial-classification")
-
-# Load model and tokenizer
-model_name = "ProsusAI/finbert"
-model = AutoModelForSequenceClassification.from_pretrained(model_name)
-tokenizer = AutoTokenizer.from_pretrained(model_name)
+from tqdm import tqdm
 
 
-def encode_data(examples, index=None):  # Add index parameter to accept the index when with_indices=True
-    """
-    Tokenizes and encodes the dataset using the specified tokenizer.
+def fine_tune_model(new_train_loader, new_val_loader, model, tokenizer, epochs=3):
+    optimizer = AdamW(model.parameters(), lr=5e-5)
 
-    Args:
-        examples (dict): A batch from the dataset.
-        index (int, optional): Index of the batch. Defaults to None.
+    for epoch in range(epochs):
+        print(f"Starting epoch {epoch + 1}/{epochs}")
 
-    Returns:
-        dict: A dictionary with tokenized and encoded data ready for model input.
-    """
-    encoded = tokenizer(examples['sentence'], padding='max_length', truncation=True, max_length=512)
-    encoded_tensors = {key: torch.tensor(val, dtype=torch.long) for key, val in encoded.items()}
-    # Debugging print statement
-    print(f"Type of input_ids in encode_data: {type(encoded_tensors['input_ids'])}")
-    return encoded_tensors
-
-
-encoded_dataset = dataset.map(encode_data, batched=True, with_indices=True)
-
-# Test the output of the encode_data function
-test_encoded = encode_data({'sentence': ["Test sentence"]})
-print(f"Test encoded input_ids type: {type(test_encoded['input_ids'])}, shape: {test_encoded['input_ids'].shape}")
-sample_after_encode = next(iter(encoded_dataset['train']))
-print(
-    f"After encode_data - input_ids: {sample_after_encode['input_ids']}, type: {type(sample_after_encode['input_ids'])}")
-
-
-def format_labels(example, index=None):  # Add index parameter here as well
-    """
-    Converts various label formats of a single example in the dataset to a tensor.
-    Handles labels that are integers, strings, or a combination of both.
-
-    Args:
-        example (dict): A single example from the dataset.
-        index (int, optional): Index of the batch. Defaults to None.
-
-    Returns:
-        dict: A dictionary with the label converted to a tensor.
-    """
-    label_map = {'negative': 0, 'neutral': 1, 'positive': 2}
-    label_str = str(example['label']).lower().strip()
-
-    # Extract integer from string if present, or map string directly
-    match = re.match(r'(\d+|\w+)', label_str)
-    if match:
-        extracted_label = match.group(1)
-        if extracted_label.isdigit():
-            label = int(extracted_label)
-        elif extracted_label in label_map:
-            label = label_map[extracted_label]
-        else:
-            raise ValueError(f"Unrecognized label format: {label_str}")
-    else:
-        raise ValueError(f"Invalid label format: {label_str}")
-
-    # Convert the label to a tensor
-    return {'labels': torch.tensor(label, dtype=torch.long)}
-
-
-# Apply encoding and label formatting
-processed_dataset = encoded_dataset.map(format_labels, with_indices=True)
-
-# After applying format_labels
-sample_after_label_format = next(iter(processed_dataset['train']))
-print(
-    f"After format_labels - labels: {sample_after_label_format['labels']}, type: {type(sample_after_label_format['labels'])}")
-
-# Split the processed dataset
-train_test_split = processed_dataset["train"].train_test_split(test_size=0.1)
-dataset_dict = DatasetDict({
-    "train": train_test_split["train"],
-    "validation": train_test_split["test"]
-})
-
-# Before feeding into DataLoader
-sample_before_loader = next(iter(dataset_dict['train']))
-print(
-    f"Before DataLoader - input_ids: {sample_before_loader['input_ids']}, type: {type(sample_before_loader['input_ids'])}")
-
-
-# Custom collate function to handle lists of tensors
-def collate_fn(batch):
-    # Debugging print statements
-    for i, item in enumerate(batch):
-        print(f"Item {i} - input_ids type: {type(item['input_ids'])}, labels type: {type(item['labels'])}")
-        if isinstance(item['input_ids'], list):
-            print(f"Item {i} - input_ids is a list, which should be converted to a tensor.")
-        if isinstance(item['labels'], int):
-            print(f"Item {i} - labels is an int, which should be converted to a tensor.")
-
-    input_ids = torch.stack([torch.tensor(item['input_ids'], dtype=torch.long) for item in batch])
-    attention_mask = torch.stack([torch.tensor(item['attention_mask'], dtype=torch.long) for item in batch])
-    labels = torch.stack([torch.tensor(item['labels'], dtype=torch.long) for item in batch])
-
-    return {
-        'input_ids': input_ids,
-        'attention_mask': attention_mask,
-        'labels': labels
-    }
-
-
-# DataLoaders with custom collate function
-train_loader = DataLoader(dataset_dict['train'], batch_size=16, shuffle=True, collate_fn=collate_fn)
-valid_loader = DataLoader(dataset_dict['validation'], batch_size=16, collate_fn=collate_fn)
-
-# Get a batch from the DataLoader
-batch = next(iter(train_loader))
-
-# Check the batch types and shapes
-print(f"DataLoader batch - input_ids type: {type(batch['input_ids'])}, shape: {batch['input_ids'].shape}")
-print(f"DataLoader batch - labels type: {type(batch['labels'])}, shape: {batch['labels'].shape}")
-
-# Optimizer
-optimizer = AdamW(model.parameters(), lr=5e-5)
-
-# Training Loop
-epochs = 3
-for epoch in range(epochs):
-    model.train()
-    for batch in train_loader:
-        print(f"Training batch - input_ids type: {type(batch['input_ids'])}, shape: {batch['input_ids'].shape}")
-        print(f"Training batch - labels type: {type(batch['labels'])}, shape: {batch['labels'].shape}")
-
-        input_ids = batch['input_ids'].to(model.device)
-        attention_mask = batch['attention_mask'].to(model.device)
-        labels = batch['labels'].to(model.device)
-
-        # Forward pass
-        outputs = model(input_ids=input_ids, attention_mask=attention_mask, labels=labels)
-        loss = outputs.loss
-
-        # Backward pass and optimization
-        loss.backward()
-        optimizer.step()
-        optimizer.zero_grad()
-
-    model.eval()
-    total_eval_loss = 0
-    for batch in valid_loader:
-        batch = {k: v.to(model.device) for k, v in batch.items()}
-        with torch.no_grad():
-            outputs = model(**batch)
+        # Training phase
+        model.train()
+        total_train_loss = 0
+        for batch in tqdm(new_train_loader, desc='Training'):
+            b_input_ids, b_input_mask, b_labels = batch
+            model.zero_grad()
+            outputs = model(b_input_ids, attention_mask=b_input_mask, labels=b_labels)
             loss = outputs.loss
-            total_eval_loss += loss.item()
+            total_train_loss += loss.item()
+            loss.backward()
+            optimizer.step()
 
-    avg_val_loss = total_eval_loss / len(valid_loader)
-    print(f'Validation loss after epoch {epoch}: {avg_val_loss}')
+        avg_train_loss = total_train_loss / len(new_train_loader)
+        print(f"Average training loss: {avg_train_loss}")
 
-# Save the fine-tuned model
-model.save_pretrained("./finetuned-finbert")
+        # Validation phase
+        model.eval()
+        total_eval_loss = 0
+        for batch in tqdm(new_val_loader, desc='Validation'):
+            b_input_ids, b_input_mask, b_labels = batch
+            with torch.no_grad():
+                outputs = model(b_input_ids, attention_mask=b_input_mask, labels=b_labels)
+                loss = outputs.loss
+                total_eval_loss += loss.item()
+
+        avg_val_loss = total_eval_loss / len(new_val_loader)
+        print(f"Validation loss: {avg_val_loss}")
+
+        # Save a checkpoint after each epoch
+        checkpoint_path = f"./finetuned-finbert-checkpoint-epoch-{epoch + 1}"
+        model.save_pretrained(checkpoint_path)
+        print(f"Checkpoint saved to {checkpoint_path}")
+
+    # Save the final fine-tuned model
+    model_path = "./finetuned-finbert"
+    model.save_pretrained(model_path)
+    tokenizer.save_pretrained(model_path)
+    print(f"Model and tokenizer saved to {model_path}")
