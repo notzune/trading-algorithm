e7554c9bba49001f1c357bdae7856568cb59d40c
zeyad rashed <brwnboy2002@gmail.com>
2023-12-11 20:44:29 -0500

    feat: made sentiment-analysis function for use in application

--- sentiment-analysis.py
+++ sentiment-analysis.py
@@ -2,66 +2,51 @@ import torch
 from transformers import AutoModelForSequenceClassification, AutoTokenizer
 import torch.nn.functional as F
 
-# Load FinBERT model and tokenizer
-model_name = "ProsusAI/finbert"
-model = AutoModelForSequenceClassification.from_pretrained(model_name)
-tokenizer = AutoTokenizer.from_pretrained(model_name)
 
-
-def preprocess(text):
+def sentiment_analysis(text, verbose=False, model_path="./finetuned-finbert", tokenizer_path="./finetuned-finbert"):
     """
-    Preprocesses the given text for sentiment analysis.
+    Performs sentiment analysis on the given text.
 
     Args:
     text (str): The text to be analyzed.
+    verbose (bool): Flag to output detailed information including probabilities.
+    model_path (str): Path to the fine-tuned model directory.
+    tokenizer_path (str): Path to the tokenizer directory.
 
     Returns:
-    torch.Tensor: The processed inputs as tensors.
+    str: The sentiment class (Negative, Neutral, Positive).
+    Optional: Prints detailed probabilities if verbose is True.
     """
+    # Load model and tokenizer
+    model = AutoModelForSequenceClassification.from_pretrained(model_path)
+    tokenizer = AutoTokenizer.from_pretrained(tokenizer_path)
+
+    # Preprocess the text
     inputs = tokenizer(text, return_tensors="pt", truncation=True, max_length=512)
-    return inputs
 
-
-def analyze_sentiment(text):
-    """
-    Analyzes the sentiment of the given text.
-
-    Args:
-    text (str): The text to be analyzed.
-
-    Returns:
-    torch.Tensor: The logits representing sentiment scores.
-    """
-    inputs = preprocess(text)
+    # Analyze sentiment
     with torch.no_grad():
         outputs = model(**inputs)
-    return outputs.logits
+    logits = outputs.logits
+    probabilities = F.softmax(logits, dim=1)
+
+    # Determine sentiment class
+    sentiment_class = torch.argmax(probabilities, dim=1).item()
+    sentiment_labels = ["Negative", "Neutral", "Positive"]
+    sentiment = sentiment_labels[sentiment_class]
+
+    # Verbose output
+    if verbose:
+        print(f"[[Negative, Neutral, Positive]]")
+        print(f"Probabilities: {probabilities.numpy()[0]}")
+        print(f"Sentiment: {sentiment}")
+
+    return sentiment
 
 
-def get_sentiment(probabilities):
-    """
-    Determines the sentiment class from the given probabilities and prints them.
-
-    Args:
-    probabilities (torch.Tensor): The probabilities tensor.
-
-    Returns:
-    int: The sentiment class (0: Negative, 1: Neutral, 2: Positive).
-    """
-    probabilities = F.softmax(probabilities, dim=1)
-    print(f"[[Negative, Neutral, Positive]]")
-    print(f"Probabilities: {probabilities}")  # Print raw probabilities
-    sentiment = torch.argmax(probabilities, dim=1)
-    return sentiment.item()
-
-
-# Example text for analysis
+# Example usage
 example_text = "The company's stock price surged following the announcement of record profits."
-
-# Analyze sentiment
-logits = analyze_sentiment(example_text)
-sentiment_class = get_sentiment(logits)
+sentiment = sentiment_analysis(example_text, verbose=True)
 
 # Output sentiment class
-sentiment_labels = ["Negative", "Neutral", "Positive"]
-print(f"Sentiment: {sentiment_labels[sentiment_class]}")
+print(f"Sentiment: {sentiment}")
